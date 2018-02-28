4937c9c5aeaeacacb3e7076ad1a19e1570b70319
Joseph Parker <jparker010190@gmail.com>
2018-02-28 02:19:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-function happyHolidaysTo(louis){
-  return "Happy Holidays, ${louis}!"
+function happyHolidaysTo(name){
+  return "Happy Holidays, ${name}!"
 }
-function happyHolidayTo(christmas,joey)
+function happyHolidayTo(,joey)
 {return "Happy ${christmas}, ${name}!"}
 function holidayCountdown(christmas, days){
   return "It's ${days} days until ${holiday}!"
